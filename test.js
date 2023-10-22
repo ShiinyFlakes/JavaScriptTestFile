bb539b2e2a1d3a9b95d8d72aa8e0557d82253d0f
magoelm <mago_elm@hotmail.com>
2023-10-22 13:52:43 +0200

    test pushing file

---
+++ test.js
@@ -0,0 +1,23 @@
+var x = 0;
+while (x < 5) {
+    if(x===1){
+        console.log("Eins");
+    }
+
+    else if(x===2){
+        console.log("Zwei");
+    }
+    
+    else if(x===3){
+        console.log("Drei");
+    }
+    
+    else if(x===4){
+        console.log("Vier");
+    }
+    else if(x===5){
+        console.log("Fünf");
+    }
+    x++;
+    
+}
